e2c65201bdfd8a4469f14e747d314297a3f56b75
YASH AUTI <yashauti5@gmail.com>
2025-09-21 17:52:36 +0530

    Update Project.java

--- src/main/java/com/portfolio/YashAuti/model/Project.java
+++ src/main/java/com/portfolio/YashAuti/model/Project.java
@@ -6,11 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "projects")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
